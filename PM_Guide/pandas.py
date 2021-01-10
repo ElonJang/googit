090106fb812a26939beb605cec36713501172bf4
장형욱 <elonj33@gmail.com>
2021-01-10 11:56:08 +0000

    I did it

--- PM_Guide/pandas.py
+++ PM_Guide/pandas.py
@@ -1,3 +1,8 @@
+# 넘파이 임포트하고
 import numpy as np
 array1 = np.array([1, 2, 3])
 print('array1 type:', type(array1))
+
+# 행렬을 정렬하기 sort()와 argsort()
+org_array = np.array([3, 1, 9, 5])
+print('원본 행렬:', org_array)
