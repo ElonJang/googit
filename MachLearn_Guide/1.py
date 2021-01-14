c3143ab79ea5262263369f9b593470a0d23e5c7a
장형욱 <elonj33@gmail.com>
2021-01-14 11:56:10 +0000

    4

--- MachLearn_Guide/1.py
+++ MachLearn_Guide/1.py
@@ -5,3 +5,12 @@ score_array = np.array([78, 95, 84, 98, 88])
 sort_indices_asc = np.argsort(score_array)
 print('성적 오름차순 정렬 시 score_array의 인덱스:', sort_indices_asc)
 print('성적 오름차순으로 name_array의 이름 출력:', name_array[sort_indices_asc])
+
+# 선형대수 연산
+
+# 행렬 내적(곱)
+
+A = np.array([[1, 2, 3], [4, 5, 6]])
+B = np.array([[7, 8], [9, 10], [11, 12]])
+dot_product = np.dot(A, B)
+print('행렬 내적 결과:\n', dot_product)
