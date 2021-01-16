49ea5187b0c11d32e02846367d1ba5866f0cb386
장형욱 <elonj33@gmail.com>
2021-01-16 14:54:19 +0000

    1

--- Inflearn_Facebook/DataAnalysis.py
+++
@@ -1,9 +0,0 @@
-# 모듈 불러오기는 파이썬-모듈 부분 보기.
-import numpy as np
-data = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
-# 이원소에 곱하기 2를 해보자.
-for i in data:
-    for j in i:
-        j = print(j)
-        
-        3&&5
