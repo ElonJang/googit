7e2c5b5e6f15b5201615ebd7a7f4bd4b720c9a56
장형욱 <elonj33@gmail.com>
2021-01-16 13:13:16 +0000

    3

--- mysite/tutorialdjango/settings.py
+++ mysite/tutorialdjango/settings.py
@@ -37,8 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'django_extensions'
-    'main',
+    'django_extensions',
+    'main'
 ]
 
 MIDDLEWARE = [
